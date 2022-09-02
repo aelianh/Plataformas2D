08a4ecaefb6b44718ff9f657f755cf22efcef753
VicCTS <95314178+VicCTS@users.noreply.github.com>
2022-09-02 09:19:01 +0200

    Etapa 2

--- Assets/Scripts/Characters/PlayerMove.cs
+++ Assets/Scripts/Characters/PlayerMove.cs
@@ -5,24 +5,58 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public float runSpeed = 2f;
+    public float jumpForce = 10f;
 
-    private Rigidbody2D body; 
-    private float horizontal;
+    private Rigidbody2D body;
+    private Animator anim;
+    private float dirX;
 
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
+        dirX = Input.GetAxisRaw("Horizontal");
+
+        if(dirX < 0)
+        {
+            anim.SetBool("walk", true);
+            transform.rotation = Quaternion.Euler(0,180,0);
+            
+        }
+        else if(dirX > 0)
+        {
+            anim.SetBool("walk", true);
+            transform.rotation = Quaternion.Euler(0,0,0);
+        }
+        else
+        {
+            anim.SetBool("walk", false); 
+        }
+
+        if(Input.GetButtonDown("Jump") && CheckGround.isGrounded)
+        {
+            body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            anim.SetBool("jump", true);
+        }
+
     }
 
     void FixedUpdate()
     {
-        body.velocity = new Vector2(horizontal * runSpeed, 0);
+        body.velocity = new Vector2(dirX * runSpeed, body.velocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Grounds")
+        {
+            anim.SetBool("jump", false);
+        }
     }
 }
